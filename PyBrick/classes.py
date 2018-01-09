914e58c60e61068004ce2a166d6ed216901305e0
Olivier Burggraaff <olivierburggraaff@gmail.com>
2018-01-09 14:48:30 +0100

    New Lot.__repr__

--- PyBrick/classes.py
+++ PyBrick/classes.py
@@ -153,8 +153,9 @@ class Lot(object):
         return cls(part, vendor, price, qty, step, lotnr)
 
     def __repr__(self):
-        return "E"+str(self.price_total)+" for "+self.part.code+" at "+self.vendor.storename.encode("ascii", "replace")+" ("+self.vendor.loc+")"
-
+        return "Lot (Part {part:8}; Price {price:>6.2f}; Vendor {vendor}, {loc})"\
+               .format(part=self.part.code, price=self.price_total,
+                       vendor=self.vendor.storename, loc=self.vendor.loc)
 
 class Vendor(object):
     def __init__(self, name, storename, loc, minbuy, preferred=[]):
